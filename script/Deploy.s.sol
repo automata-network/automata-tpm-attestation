730cee80d31d9befdd1a2c7a502bea1b864db5d7
Preston Ong <preston4896@me.com>
2025-07-29 17:14:19 +0800

    deployment script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: Apache2
+// Automata Contracts
+pragma solidity ^0.8.15;
+
+import {console} from "forge-std/console.sol";
+import {P256Configuration} from "./utils/P256Configuration.sol";
+import {DeploymentConfig} from "./utils/DeploymentConfig.sol";
+import "./utils/Salt.sol";
+
+import {TpmAttestation} from "../src/TpmAttestation.sol";
+
+contract Deploy is DeploymentConfig, P256Configuration {
+    address owner = vm.envAddress("OWNER");
+
+    modifier broadcast() {
+        vm.startBroadcast(owner);
+        _;
+        vm.stopBroadcast();
+    }
+
+
+    function deployTpmAttestation() public broadcast {
+        // deploy the TpmAttestation implementation
+        TpmAttestation tpmAttestation = new TpmAttestation{salt: TPM_ATTESTATION_SALT}(owner, simulateVerify());
+
+        console.log("TpmAttestation deployed at:", address(tpmAttestation));
+
+        // write the implementation address to JSON
+        writeToJson("TpmAttestation", address(tpmAttestation));
+    }
+}
---
+++ script/utils/Salt.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.0;
+
+bytes32 constant TPM_ATTESTATION_SALT = keccak256(bytes("TPM_ATTESTATION_SALT"));
